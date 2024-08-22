a746cccefe6db77a9564addf0550f6f2ef403dcf
Nethmini Prabodha <nethunethmini256@gmail.com>
2024-08-22 11:21:35 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,69 +9,72 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-black text-gray-300 h-[100px] w-full fixed top-0 left-0 z-50">
-      <div className="max-w-[1200px] mx-auto flex justify-between items-center h-full">
+    <div className="bg-black text-gray-300 h-[80px] w-full fixed top-0 left-0 z-50">
+      <div className="max-w-[1200px] mx-auto flex justify-between items-center h-full px-4">
+        {/* Logo */}
         <h1 className="text-3xl font-bold primary-color">Nethmini Prabodha</h1>
-        <ul className="hidden md:flex">
-          <li className="p-5  hover:text-blue-400">
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6">
+          <li className="hover:text-blue-400">
             <a href="#hero">Home</a>
           </li>
-          <li className="p-5 hover:text-blue-400">
+          <li className="hover:text-blue-400">
             <a href="#skills">Skills</a>
           </li>
-          <li className="p-5 hover:text-blue-400">
+          <li className="hover:text-blue-400">
             <a href="#about">About</a>
           </li>
-          <li className="p-5 hover:text-blue-400">
+          <li className="hover:text-blue-400">
             <a href="#projects">My Projects</a>
           </li>
-          <li className="p-5 hover:text-blue-400">
+          <li className="hover:text-blue-400">
             <a href="#contact">Contact</a>
           </li>
         </ul>
 
-        <div onClick={handleNav} className="block md:hidden mr-6">
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {/* Mobile Menu Toggle Button */}
+        <div onClick={handleNav} className="block md:hidden z-50 cursor-pointer">
+          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
         </div>
-      </div>
 
-      {/* Mobile Menu */}
-      <div
-        className={
-          nav
-            ? "fixed h-full left-0 top-0 w-[60%] bg-[#202121] z-10 ease-in-out duration-500"
-            : "fixed left-[-100%]"
-        }
-      >
-        <h1 className="text-3xl primary-color m-4">Nethmini Prabodha</h1>
-        <ul className="pt-8 text-2xl">
-          <li className="p-2">
-            <a href="#hero" onClick={handleNav}>
-              Home
-            </a>
-          </li>
-
-          <li className="p-2">
-            <a href="#skills" onClick={handleNav}>
-              Skills
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#about" onClick={handleNav}>
-              About
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#projects" onClick={handleNav}>
-              My Projects
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#contact" onClick={handleNav}>
-              Contact
-            </a>
-          </li>
-        </ul>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? "md:hidden fixed left-0 top-0 w-[60%] h-full bg-[#202121] text-gray-300 p-8 z-40 ease-in-out duration-500"
+              : "fixed left-[-100%] top-0 p-8 h-full ease-in-out duration-500 z-40"
+          }
+        >
+          <h1 className="text-3xl font-bold primary-color">Nethmini Prabodha</h1>
+          <ul className="space-y-6 text-2xl">
+            <li>
+              <a href="#hero" onClick={handleNav}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#skills" onClick={handleNav}>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={handleNav}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onClick={handleNav}>
+                My Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={handleNav}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
